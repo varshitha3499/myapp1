a34347c76989aff793c7b8e3f8fee2e1e85df869
varshitha3499 <kanneboyinavarshitha@gmail.com>
2025-04-23 21:56:50 +0530

    Update app.py

--- Doctorappointment/app.py
+++ Doctorappointment/app.py
@@ -285,4 +285,4 @@ def post_prescription(appointment_id):
     return redirect(url_for('doctor_page'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
